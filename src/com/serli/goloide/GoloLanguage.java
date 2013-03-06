4e265307f7f761a2c63f65846e2433f256e5469c
David Festal <david.festal@serli.com>
2013-03-06 15:10:28 +0100

    Register the Parser and StructureScanner

--- src/com/serli/goloide/GoloLanguage.java
+++ src/com/serli/goloide/GoloLanguage.java
@@ -1,9 +1,13 @@
 package com.serli.goloide;
 
+import com.serli.goloide.structure.GoloStructureScanner;
 import com.serli.goloide.lexer.GoloTokenId;
+import com.serli.goloide.parser.GoloParser;
 import org.netbeans.api.lexer.Language;
+import org.netbeans.modules.csl.api.StructureScanner;
 import org.netbeans.modules.csl.spi.DefaultLanguageConfig;
 import org.netbeans.modules.csl.spi.LanguageRegistration;
+import org.netbeans.modules.parsing.spi.Parser;
 
 /**
  *
@@ -20,7 +24,26 @@ public class GoloLanguage extends DefaultLanguageConfig {
 
     @Override
     public String getDisplayName() {
-        return "SJ";
+        return "Golo";
     }
 
+    @Override
+    public String getPreferredExtension() {
+        return "golo";
+    }
+    
+    @Override
+    public Parser getParser() {
+        return new GoloParser();
+    }
+    
+    @Override
+    public StructureScanner getStructureScanner() {
+        return new GoloStructureScanner();
+    }
+
+    @Override
+    public boolean hasStructureScanner() {
+        return true;
+    }
 }
