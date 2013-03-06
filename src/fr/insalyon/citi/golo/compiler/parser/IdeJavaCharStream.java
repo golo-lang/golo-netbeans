f4d221caee1a34196d8fee014df2790f51da1eee
David Festal <david.festal@serli.com>
2013-03-06 15:07:37 +0100

    Derived JavaCharStream to manage the new token attributes

---
+++ src/fr/insalyon/citi/golo/compiler/parser/IdeJavaCharStream.java
@@ -0,0 +1,56 @@
+package fr.insalyon.citi.golo.compiler.parser;
+
+import java.io.IOException;
+import java.io.Reader;
+
+/**
+ *
+ * @author david
+ */
+public class IdeJavaCharStream extends JavaCharStream implements GoloParserTokenManager.TokenCompleter {
+    Token lastToken = null;
+    private int currentOffset = 0;
+    private int tokenStart = 0;
+
+    public IdeJavaCharStream(Reader reader) {
+        super(reader);
+    }
+
+    @Override
+    public char readChar() throws IOException {
+        char result = super.readChar();
+            currentOffset++;
+        return result;
+    }
+
+    @Override
+    public char BeginToken() throws IOException {
+        tokenStart = currentOffset;
+        char result = super.BeginToken();
+        if (tokenStart == currentOffset) {
+            currentOffset++;
+        }
+        return result;
+    }
+
+    @Override
+    public void backup(int amount) {
+        super.backup(amount);
+        currentOffset -= amount;
+    }
+
+    @Override
+    public void ReInit(Reader dstream) {
+        super.ReInit(dstream);
+        currentOffset = 0;
+    }
+
+    @Override
+    public void completeToken(Token token) {
+        token.startOffset = tokenStart;
+        token.endOffset = currentOffset;
+        token.previousToken = lastToken;
+        lastToken = token;
+    }
+
+}
