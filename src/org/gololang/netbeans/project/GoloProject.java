2fd0d9c4bfb6acab30f67ef5a756d226ac3c47ef
Julien DERAY <julien@deray.fr>
2014-09-22 12:30:07 +0200

    Add project's logical view

--- src/org/gololang/netbeans/project/GoloProject.java
+++ src/org/gololang/netbeans/project/GoloProject.java
@@ -17,7 +17,9 @@
 
 package org.gololang.netbeans.project;
 
+import java.awt.Image;
 import java.beans.PropertyChangeListener;
+import javax.swing.Action;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.event.ChangeListener;
@@ -28,10 +30,20 @@ import org.netbeans.api.project.SourceGroup;
 import org.netbeans.api.project.Sources;
 import org.netbeans.spi.project.ProjectState;
 import org.netbeans.spi.project.support.GenericSources;
+import org.netbeans.spi.project.ui.LogicalViewProvider;
+import org.netbeans.spi.project.ui.support.CommonProjectActions;
 import org.openide.filesystems.FileObject;
+import org.openide.loaders.DataFolder;
+import org.openide.loaders.DataObjectNotFoundException;
+import org.openide.nodes.AbstractNode;
+import org.openide.nodes.Children;
+import org.openide.nodes.FilterNode;
+import org.openide.nodes.Node;
+import org.openide.util.Exceptions;
 import org.openide.util.ImageUtilities;
 import org.openide.util.Lookup;
 import org.openide.util.lookup.Lookups;
+import org.openide.util.lookup.ProxyLookup;
 
 /**
  *
@@ -64,7 +76,8 @@ public class GoloProject implements Project {
             lkp = Lookups.fixed(new Object[]{
             
                 new Info(),
-                new GoloSources()
+                new GoloSources(),
+                new CustomerProjectLogicalView(this)
             
             });
         }
@@ -169,4 +182,85 @@ public class GoloProject implements Project {
         public void removeChangeListener(ChangeListener cl) {
         }
     }
+    
+    class CustomerProjectLogicalView implements LogicalViewProvider {
+
+    @StaticResource()
+    public static final String GOLO_ICON = "org/gololang/netbeans/golo_icon_16px.png";
+
+    private final GoloProject project;
+
+    public CustomerProjectLogicalView(GoloProject project) {
+        this.project = project;
+    }
+
+    @Override
+    public Node createLogicalView() {
+        try {
+            //Obtain the project directory's node:
+            FileObject projectDirectory = project.getProjectDirectory();
+            DataFolder projectFolder = DataFolder.findFolder(projectDirectory);
+            Node nodeOfProjectFolder = projectFolder.getNodeDelegate();
+            //Decorate the project directory's node:
+            return new ProjectNode(nodeOfProjectFolder, project);
+        } catch (DataObjectNotFoundException donfe) {
+            Exceptions.printStackTrace(donfe);
+            //Fallback-the directory couldn't be created -
+            //read-only filesystem or something evil happened
+            return new AbstractNode(Children.LEAF);
+        }
+    }
+
+    private final class ProjectNode extends FilterNode {
+
+        final GoloProject project;
+
+        public ProjectNode(Node node, GoloProject project) 
+            throws DataObjectNotFoundException {
+            super(node,
+                new FilterNode.Children(node),
+                new ProxyLookup(
+                    new Lookup[]{
+                        Lookups.singleton(project),
+                        node.getLookup()
+                    }
+                )
+            );
+            this.project = project;
+        }
+
+        @Override
+        public Action[] getActions(boolean arg0) {
+            return new Action[]{
+                CommonProjectActions.newFileAction(),
+                CommonProjectActions.copyProjectAction(),
+                CommonProjectActions.deleteProjectAction(),
+                CommonProjectActions.closeProjectAction()
+            };
+        }
+
+        @Override
+        public Image getIcon(int type) {
+            return ImageUtilities.loadImage(GOLO_ICON);
+        }
+
+        @Override
+        public Image getOpenedIcon(int type) {
+            return getIcon(type);
+        }
+
+        @Override
+        public String getDisplayName() {
+            return project.getProjectDirectory().getName();
+        }
+
+    }
+
+    @Override
+    public Node findPath(Node root, Object target) {
+        //leave unimplemented for now
+        return null;
+    }
+
+}
 }
