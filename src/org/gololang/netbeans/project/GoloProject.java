16979b1ac274c8795a1c4b781fefddd26f248af0
Julien DERAY <julien@deray.fr>
2014-09-22 10:30:50 +0200

    Can now open golo projects

---
+++ src/org/gololang/netbeans/project/GoloProject.java
@@ -0,0 +1,99 @@
+/*
+ *  Copyright 2013 SERLI (www.serli.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ * 
+ */
+
+package org.gololang.netbeans.project;
+
+import java.beans.PropertyChangeListener;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import org.netbeans.api.annotations.common.StaticResource;
+import org.netbeans.api.project.Project;
+import org.netbeans.api.project.ProjectInformation;
+import org.netbeans.spi.project.ProjectState;
+import org.openide.filesystems.FileObject;
+import org.openide.util.ImageUtilities;
+import org.openide.util.Lookup;
+import org.openide.util.lookup.Lookups;
+
+/**
+ *
+ * @author Julien Déray
+ */
+public class GoloProject implements Project {
+
+    private final FileObject projectDir;
+    private final ProjectState state;
+    private Lookup lkp;
+    
+    GoloProject(FileObject dir, ProjectState state) {
+        this.projectDir = dir;
+        this.state = state;
+    }
+
+    @Override
+    public FileObject getProjectDirectory() {
+        return projectDir;
+    }
+
+    @Override
+    public Lookup getLookup() {
+        if (lkp == null) {
+            lkp = Lookups.fixed(new Object[]{
+            
+                new Info()
+            
+            });
+        }
+        return lkp;
+    }
+
+    public final class Info implements ProjectInformation {
+    
+        @StaticResource()
+        public static final String GOLO_ICON = "org/gololang/netbeans/golo_icon_16px.png";
+
+        @Override
+        public Icon getIcon() {
+            return new ImageIcon(ImageUtilities.loadImage(GOLO_ICON));
+        }
+
+        @Override
+        public String getName() {
+            return getProjectDirectory().getName();
+        }
+
+        @Override
+        public String getDisplayName() {
+            return getName();
+        }
+
+        @Override
+        public void addPropertyChangeListener(PropertyChangeListener pcl) {
+            //do nothing, won't change
+        }
+
+        @Override
+        public void removePropertyChangeListener(PropertyChangeListener pcl) {
+            //do nothing, won't change
+        }
+
+        @Override
+        public Project getProject() {
+            return GoloProject.this;
+        }
+    }
+}
